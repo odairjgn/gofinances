f3b1790bb462f012ed8f629f9dc35569d5c67524
Odair <odairjgn@outlook.com>
2021-06-10 23:59:35 -0300

    Ajustes

---
+++ src/screens/Dashboard/index.tsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import { HighlightCard } from '../../components/HighlightCard';
+import {
+    Container,
+    Header,
+    UserWrapper,
+    UserInfo,
+    Photo,
+    User,
+    UserGreeting,
+    UserName,
+    Icon,
+    HighlightCards
+} from './styles';
+
+export function Dashboard() {
+    return (
+        <Container>
+            <Header>
+                <UserWrapper>
+                    <UserInfo>
+                        <Photo
+                            source={{ uri: 'https://avatars.githubusercontent.com/u/26652713?v=4' }}
+                        />
+                        <User>
+                            <UserGreeting>Olá, </UserGreeting>
+                            <UserName>Marilene</UserName>
+                        </User>
+                    </UserInfo>
+                    <Icon name='power' />
+                </UserWrapper>
+            </Header>
+            <HighlightCards
+            >
+                <HighlightCard />
+                <HighlightCard />
+                <HighlightCard />
+            </HighlightCards>
+        </Container>
+    )
+}
