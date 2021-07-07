d46eb4c58fe793c38ba5b9eb513785217c005315
Odair <odairjgn@outlook.com>
2021-07-06 23:21:02 -0300

    Novas implementações

--- App.tsx
+++ App.tsx
@@ -10,7 +10,7 @@ import {
 } from '@expo-google-fonts/poppins';
 
 import theme from './src/global/styles/theme';
-import { Dashboard } from './src/screens/Dashboard';
+import { CategorySelect } from './src/screens/CategorySelect';
 
 export default function App() {
 
@@ -26,7 +26,7 @@ export default function App() {
 
   return (
     <ThemeProvider theme={theme}>
-      <Dashboard />
+      <CategorySelect category={''} setCategory={(x) => { }} closeSelectCategory={() => { }} />
     </ThemeProvider>
   )
 }
---
+++ src/screens/Register/index.tsx
@@ -0,0 +1,59 @@
+import React, { useState } from 'react';
+import { Input } from '../../components/Form/Input';
+import { Button } from '../../components/Form/Button';
+import { TransactionTypeButton } from '../../components/Form/TransactionTypeButton';
+import { CategorySelect } from '../../components/Form/CategorySelect';
+
+import {
+    Container,
+    Header,
+    Title,
+    Form,
+    Fields,
+    TransactionsTypes
+} from './styles';
+
+export function Register() {
+    const [transactionType, setTransactionType] = useState('');
+
+    function handleTransactionTypes(type: 'up' | 'down') {
+        setTransactionType(type);
+    }
+
+    return (
+        <Container>
+            <Header>
+                <Title>Cadastro</Title>
+            </Header>
+
+            <Form>
+                <Fields>
+                    <Input
+                        placeholder="Nome"
+                    />
+                    <Input
+                        placeholder="Preço"
+                    />
+                    <TransactionsTypes>
+                        <TransactionTypeButton
+                            type="up"
+                            title="Income"
+                            onPress={() => handleTransactionTypes('up')}
+                            isActive={transactionType === 'up'}
+                        />
+                        <TransactionTypeButton
+                            type="down"
+                            title="Outcome"
+                            onPress={() => handleTransactionTypes('down')}
+                            isActive={transactionType === 'down'}
+                        />
+                    </TransactionsTypes>
+
+                    <CategorySelect title="Categoria" />
+                </Fields>
+
+                <Button title="Enviar" />
+            </Form>
+        </Container>
+    );
+}
