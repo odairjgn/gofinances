5c068870fd82ced36edf5f44b42939196d9b1189
Odair <odairjgn@outlook.com>
2021-07-29 22:30:02 -0300

    Conslusão unidade

--- App.tsx
+++ App.tsx
@@ -16,6 +16,7 @@ import {
 
 import theme from './src/global/styles/theme';
 import { AppRoutes } from './src/routes/app.routes';
+import { StatusBar } from 'react-native';
 
 export default function App() {
 
@@ -32,6 +33,7 @@ export default function App() {
   return (
     <ThemeProvider theme={theme}>
       <NavigationContainer>
+        <StatusBar barStyle="light-content" />
         <AppRoutes />
       </NavigationContainer>
     </ThemeProvider>
