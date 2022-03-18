cb6fd4e40f1134c73c839432ba81799da82e2129
Odair José Gambert Nossa <odairjgn@outlook.com>
2022-03-18 00:25:00 -0300

    Ajustes

---
+++ src/__tests__/screens/profile.spec.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { render } from "@testing-library/react-native";
+
+import { Profile } from "../../screens/Profile";
+
+test("Check if show correctly user input name placeholder", () => {
+  const { getByPlaceholderText } = render(<Profile />);
+  const inputName = getByPlaceholderText("Nome");
+  expect(inputName).toBeTruthy();
+});
