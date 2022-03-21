2e12795b31148ed1340eccf5c6db724d794499fc
Odair José Gambert Nossa <odairjgn@outlook.com>
2022-03-20 22:50:10 -0300

    Ajustes

--- src/__tests__/screens/profile.spec.tsx
+++ src/__tests__/screens/profile.spec.tsx
@@ -3,8 +3,24 @@ import { render } from "@testing-library/react-native";
 
 import { Profile } from "../../screens/Profile";
 
-test("Check if show correctly user input name placeholder", () => {
-  const { getByPlaceholderText } = render(<Profile />);
-  const inputName = getByPlaceholderText("Nome");
-  expect(inputName).toBeTruthy();
+describe("Profile Screen", () => {
+  it("Should have placeholder correctly in user name input", () => {
+    const { getByPlaceholderText } = render(<Profile />);
+    const inputName = getByPlaceholderText("Nome");
+    expect(inputName).toBeTruthy();
+  });
+
+  it("Should be loaded user data", () => {
+    const { getByTestId } = render(<Profile />);
+    const inputName = getByTestId("input-name");
+    const inputSurname = getByTestId("input-surname");
+    expect(inputName.props.value).toEqual("Didi");
+    expect(inputSurname.props.value).toEqual("Mocó");
+  });
+
+  it("Should exist title correctly", () => {
+    const { getByTestId } = render(<Profile />);
+    const textTitle = getByTestId("text-title");
+    expect(textTitle.props.children).toContain("Perfil");
+  });
 });
